5728deb5c618306f04134208e29431172167d763
Dario Vidas <dario.vidas92@gmail.com>
2022-12-17 15:42:48 -0800

    Phaser skeleton

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('index');
+import * as Phaser from 'phaser';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+let game = new Phaser.Game(config);
+
+function preload(): void {
+
+}
+
+function create(): void {
+
+}
+
+function update(): void {
+  console.log(Date.now());
+}
